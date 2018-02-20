df6797f5ae6b8a2de5e813fadbe35b032849303b
Eddie Qi <eddieqi128@gmail.com>
2018-02-20 19:24:13 +1100

    use bodyParser.urlencoded

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const url = require("url");
 const bodyParser = require("body-parser");
 
 module.exports = function(app) {
-	app.use(bodyParser.text({ type: "application/x-www-form-urlencoded" }));
+	app.use(bodyParser.urlencoded());
 
 	app.post("/login", (req, res) => {
 		console.log(req.body);
