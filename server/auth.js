ba3421948ddc2cc99ccb21c5373875cd2452d8d5
Eddie Qi <eddieqi128@gmail.com>
2018-02-24 22:21:32 +1100

    Removed old async implementation, using promises now

--- server/auth.js
+++ server/auth.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcrypt");
 const url = require("url");
 const bodyParser = require("body-parser");
-const sqlite = require("sqlite3");
+const sqlite = require("sqlite");
 const ejs = require("ejs");
 
 module.exports = function(app) {
@@ -12,22 +12,17 @@ module.exports = function(app) {
 		.get((req, res) => {
 			res.render("login", { message: "" });
 		})
-		.post((req, res) => {
-			db = new sqlite.Database("data/db.sqlite", () => {
-				db.get("SELECT * FROM users WHERE identifier = ?", req.body.identifier, (err, row) => {
-					db.close()
+		.post(async (req, res) => {
+			var db = await sqlite.open("data/db.sqlite");
 
-					if (err) {
-						console.error(err);
-						return res.render("login", { message: "Server error: " + err });
-					}
+			row = await db.get("SELECT * FROM users WHERE identifier = ?", req.body.identifier);
 
-					if (row && row.password === req.body.password) {
-						return res.render("login", { message: "Success" });
-					} 
-					return res.render("login", { message: "Incorrect username or password" });
-				});
-			});
+			db.close()
+
+			if (row && row.password === req.body.password) {
+				return res.render("login", { message: "Success" });
+			}
+			return res.render("login", { message: "Incorrect username or password" });
 		});
 
 	app.route("/register")
