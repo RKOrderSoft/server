79dd3338d3fcea1467df129d2a15dcdb30e6c12d
Eddie Qi <eddieqi128@gmail.com>
2018-05-15 18:48:07 +1000

    more legit /api/test

--- server/api.js
+++ server/api.js
@@ -2,14 +2,21 @@
 // I don't even know what REST means tbh
 // i think it's one of those buzzwords
 const component = "api";
+const acceptedClients = ["dotnet", "js"]
 
 module.exports = function (app, db, auth, sh) {
 	app.post("/api/test", (req, res) => {
 		sh.log("POST /api/test/ from " + req.ip, component, true);
-		console.log(req.body);
-		if (req.body && req.body.name == "test") {
-			return res.json({ works: "yes" });
+		//console.log(req.body);
+		if (req.body && req.body.client && acceptedClients.indexOf(req.body.client) > -1) {
+			return res.json({ 
+				version: "OrderSoft server v1.0.0",
+				acceptedClient: true
+			});
 		}
-		return res.json({ works: "nope ya fucked up" });
+		return res.json({ 
+			version: "OrderSoft server v1.0.0",
+			acceptedClient: false
+		});
 	});
 }
