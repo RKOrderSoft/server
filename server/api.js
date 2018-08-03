fdd74ae9e24995a01206e72eb3f46485ec350484
Eddie Qi <eddieqi128@gmail.com>
2018-08-03 23:05:51 +1000

    Create new checkAccessLevel helpt

--- server/api.js
+++ server/api.js
@@ -68,32 +68,16 @@ module.exports = function (app, db, auth, sessions, sh) {
 	//   Used to retrieve order information
 	app.post("/api/getorder", async (req, res) => {
 		sh.log("POST /api/getorder/ from " + req.ip, component, true);
-		const REQD_ACCESSLVL = 0;
 
 		// Check client name
 		if (!checkAcceptedClient(req, res)) return;
 
-		var resBody = {};
-		var accessLevel = -1;
-		
-		// Get access level of authenticated user
-		try {
-			accessLevel = await sessions.getAccessLevel(req.get("sessionId"));
-		} catch (err) {
-			// Error retrieving session data
-			res.status(401);
-			resBody.reason = err.toString();
-		}
+		// Check access level
+		if (!(await checkAccessLevel(sessions, req, res, 0))) return;
 
-		// Check access conditions
-		if (accessLevel == -1) {
-			// error retrieving access level
-			// empty if statement to prevent further conditionals from being evaluated
-		} else if (accessLevel < REQD_ACCESSLVL) {
-			// access level too low
-			res.status(403);
-			resBody.reason = `Access level ${accessLevel} is too low; minimum ${REQD_ACCESSLVL}`
-		} else if (!(req.body.orderId) && !(req.body.tableNumber)) {
+		var resBody = {};
+
+		if (!(req.body.orderId) && !(req.body.tableNumber)) {
 			// malformed request
 			res.status(400);
 			resBody.reason = "Either orderId or tableNumber must be provided.";
@@ -137,8 +121,11 @@ module.exports = function (app, db, auth, sessions, sh) {
 	app.post("/api/setorder", async (req, res) => {
 		sh.log("POST /api/setorder from " + req.ip, component, true);
 		
+		// Check client
 		if (!checkAcceptedClient(req, res)) return;
-		if (!checkAccessLevel(req, res, 0)) return;
+
+		// Check access level
+		if (!(await checkAccessLevel(sessions, req, res, 0))) return;
 	});
 
 	// /api/openOrders
@@ -150,70 +137,58 @@ module.exports = function (app, db, auth, sessions, sh) {
 		// Check client name
 		if (!checkAcceptedClient(req, res)) return;
 
-		var accessLevel = -1;
+		// Check access level
+		if (!(await checkAccessLevel(sessions, req, res, 0))) return;
+
 		var resBody = {};
-		
-		// Get access level of authenticated user
+
+		var rows;
+
 		try {
-			accessLevel = await sessions.getAccessLevel(req.get("sessionId"));
-		} catch (err) {
-			// Error retrieving session data
-			res.status(401);
-			resBody.reason = err.toString();
+			var queryText = "SELECT orderId FROM orders WHERE orderComplete = 0"
+			rows = await db.all(queryText);
+		} catch (ex) {
+			sh.log("Error: " + ex.toString(), component);
 		}
 
-		if (accessLevel >= REQD_ACCESSLVL) {
-			var rows;
+		var orderIds = rows.map(row => row.orderId);
 
-			try {
-				var queryText = "SELECT orderId FROM orders WHERE orderComplete = 0"
-				rows = await db.all(queryText);
-			} catch (ex) {
-				sh.log("Error: " + ex.toString(), component);
-			}
-
-			var orderIds = rows.map(row => row.orderId);
-
-			resBody.openOrders = orderIds;
-			res.status(200);
-		} else if (accessLevel == -1) {
-			// error getting access level, allow execution through
-		} else {
-			resBody.reason = `Access level ${accessLevel} is too low; minimum ${REQD_ACCESSLVL}`;
-			res.status(403);
-		}
+		resBody.openOrders = orderIds;
+		res.status(200);
 
 		res.json(buildResponse(resBody));
 	});
 }
 
-function checkAccessLevel(req, res, requiredLevel) {
-	// Get session id from req
-	var sessionId = req.get("sessionId");
-	if (!sessionId) {
-		// No sessionId was sent. respond with 401 and return false
-		res.status(401);
-		res.json(buildResponse({ reason: "No sessionId was sent" }));
-		return false;
-	}
+function checkAccessLevel(sessions, req, res, requiredLevel) {
+	return new Promise(async (resolve, cancel) => {
+		// Get session id from req
+		var sessionId = req.get("sessionId");
+		if (!sessionId) {
+			// No sessionId was sent. respond with 401 and return false
+			res.status(401);
+			res.json(buildResponse({ reason: "No sessionId was sent" }));
+			resolve(false);
+		}
 
-	// Get access level from database
-	var accessLevel;
-	try {
-		accessLevel = await sessions.getAccessLevel(sessionId);
-	} catch (ex) {
-		res.status(401);
-		res.json(buildResponse({ reason: `Error: ${ex.toString()}` }));
-		return false;
-	}
+		// Get access level from database
+		var accessLevel;
+		try {
+			accessLevel = await sessions.getAccessLevel(sessionId);
+		} catch (ex) {
+			res.status(401);
+			res.json(buildResponse({ reason: `Error: ${ex.toString()}` }));
+			resolve(false);
+		}
 
-	// Check access level
-	if (accessLevel < requiredLevel) {
-		res.status(403);
-		res.json(buildResponse({ reason: `Access level ${accessLevel} is too low; minimum ${REQD_ACCESSLVL}` }));
-		return false;
-	}
-	return true;
+		// Check access level
+		if (accessLevel < requiredLevel) {
+			res.status(403);
+			res.json(buildResponse({ reason: `Access level ${accessLevel} is too low; minimum ${REQD_ACCESSLVL}` }));
+			resolve(false);
+		}
+		resolve(true);
+	});
 }
 
 function checkAcceptedClient(req, res) {
